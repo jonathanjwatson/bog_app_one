5d32d812d76c9389b0d1f97546ca73aacedf7d77
jwats287 <jwats287@users.noreply.github.com>
2017-09-06 16:05:10 -0400

    Finished section 4

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -63,6 +63,22 @@ class CreaturesController < ApplicationController
         # redirect_to creature_path(creature) is equivalent to:
         # redirect_to "/creatures/#{creature.id}"
       end
+      def destroy
+      # get the creature id from the url params
+      creature_id = params[:id]
+  
+      # use `creature_id` to find the creature in the database
+      # and save it to a variable
+      creature = Creature.find_by_id(creature_id)
+  
+      # destroy the creature
+      creature.destroy
+  
+      # redirect to creatures index
+      redirect_to creatures_path
+      # redirect_to creatures_path is equivalent to:
+      # redirect_to "/creatures"
+    end
       
       private
       
