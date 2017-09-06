b208d181baad5d0758b9e2b3be5a236065fdaa85
jwats287 <jwats287@users.noreply.github.com>
2017-09-06 16:01:45 -0400

    End of part 3

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -36,5 +36,39 @@ class CreaturesController < ApplicationController
         # render the show view (it has access to instance variable)
         # remember the default behavior is to render :show
       end
+      def edit
+        # get the creature id from the url params
+        creature_id = params[:id]
+    
+        # use `creature_id` to find the creature in the database
+        # and save it to an instance variable
+        @creature = Creature.find_by_id(creature_id)
+    
+        # render the edit view (it has access to instance variable)
+        # remember the default behavior is to render :edit
+      end
+      def update
+        # get the creature id from the url params
+        creature_id = params[:id]
+    
+        # use `creature_id` to find the creature in the database
+        # and save it to an instance variable
+        creature = Creature.find_by_id(creature_id)
+    
+        # update the creature
+        creature.update_attributes(creature_params)  # this now calls the private method below!
+    
+        # redirect to show page for the updated creature
+        redirect_to creature_path(creature)
+        # redirect_to creature_path(creature) is equivalent to:
+        # redirect_to "/creatures/#{creature.id}"
+      end
+      
+      private
+      
+      def creature_params
+        # whitelist params return whitelisted version
+        params.require(:creature).permit(:name, :description) 
+      end
     
     end
